2297415ceec76725d2d8372961f2ec0c41dbdb5d
Jon Greazel <jdgreazel@gmail.com>
2022-06-26 15:09:41 -0500

    import tailwind

--- src/PokemonCard.jsx
+++ src/PokemonCard.jsx
@@ -3,27 +3,27 @@ import capitalize from "lodash.capitalize";
 
 import usePokemon from "./usePokemon";
 
-const Card = styled.div`
-  width: 160px;
-  height: 160px;
-  background: #97bc62ff;
-  text-align: center;
-  border-radius: 8px;
-  color: #2c5f2d;
-`;
+// const Card = styled.div`
+//   width: 160px;
+//   height: 160px;
+//   background: #97bc62ff;
+//   text-align: center;
+//   border-radius: 8px;
+//   color: #2c5f2d;
+// `;
 
 const PokemonCard = ({ pokemon, onClick }) => {
   const { pokemon: pokemonData, loading } = usePokemon(pokemon);
 
   if (loading) {
-    return <Card>Loading...</Card>;
+    return <div>Loading...</div>;
   }
 
   return (
-    <Card onClick={() => onClick(pokemonData)}>
+    <div onClick={() => onClick(pokemonData)}>
       <img src={pokemonData.sprites.front_default} alt={pokemon} />
       <h3>{capitalize(pokemon)}</h3>
-    </Card>
+    </div>
   );
 };
 
